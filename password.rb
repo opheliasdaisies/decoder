addb9cf8395da3b52e12d140531ddef05e65d70e
Sara Gorecki <sgorecki@gmail.com>
2013-11-18 09:33:09 -0500

    create decoder for password

--- password.rb
+++ password.rb
@@ -1,20 +1,37 @@
-the new wifi network is called def_wifi
+# the new wifi network is called def_wifi
 
-your challenge will be to decrypt the wifi password
-here is the encoder
+# your challenge will be to decrypt the wifi password
+# here is the encoder
+
+# def encode(password)
+# 	pass_as_string = password.to_s
+# 	password_array = pass_as_string.split("_")
+# 	new_password_array = []
+# 	new_password_array[0] = password_array.last
+# 	new_password_array[3] = password_array.first
+# 	new_password_array[1] = password_array[1]
+# 	new_password_array.compact!
+# 	joined_password = new_password_array.join(" ")
+# 	joined_password.gsub!("e", "blake")
+# 	joined_password.gsub!(" ", "ashley")
+# 	joined_password
+# end
+
+# the secret password is "intblakernblaketsashleythblakeashleyall"
+
+secret = "intblakernblaketsashleythblakeashleyall"
 
 def encode(password)
-	pass_as_string = password.to_s
-	password_array = pass_as_string.split("")
+	gsub_password = password.gsub("ashley", " ")
+	gsub_password.gsub!("blake", "e")
+	array_password = gsub_password.split
+	#[internets, the, all]
 	new_password_array = []
-	new_password_array[0] = password_array.last
-	new_password_array[3] = password_array.first
-	new_password_array[1] = password_array[1]
-	new_password_array.compact!
-	joined_password = new_password_array.join(" ")
-	joined_password.gsub!("e", "blake")
-	joined_password.gsub!(" ", "ashley")
-	joined_password
+	new_password_array[1] = array_password[1]
+	new_password_array[0] = array_password.last
+	new_password_array[3] = array_password.first
+	pass_string = new_password_array.join("_")
+	pass_as_sym = pass_string.to_sym
 end
 
-the secret password is "intblakernblaketsashleythblakeashleyall"
+puts encode(secret)
